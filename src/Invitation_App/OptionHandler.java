d35876927aa8918cc0ca0aab14611d6e9f83f874
brindaban <brindaba@thoughtworks.com>
2016-03-10 19:36:27 +0530

    I am showing the result as per requirment.

--- src/Invitation_App/OptionHandler.java
+++ src/Invitation_App/OptionHandler.java
@@ -60,11 +60,28 @@ public class OptionHandler {
         return result;
     }
 
+    private String getSpaces(int amountOfSpace){
+        String space = "";
+        for (int i=0;i<amountOfSpace;i++)
+            space += " ";
+        return space;
+    }
+
+    private String[] drawPatternWithValues(String[] splittedData){
+        String firstAndLastLine = "+----------------------------------+";
+        String thirdLine = "|----------------------------------|";
+        String secondLine = "| "+splittedData[0]+getSpaces(firstAndLastLine.length()-splittedData[0].length()-3)+"|";
+        String forthLine = "| "+splittedData[1]+getSpaces(firstAndLastLine.length()-splittedData[1].length()-3)+"|";
+        String fifthLine = "| "+splittedData[2]+getSpaces(firstAndLastLine.length()-splittedData[2].length()-3)+"|";
+        return new String[]{firstAndLastLine, secondLine, thirdLine, forthLine, fifthLine, firstAndLastLine+"\n"};
+    }
+
     private void printTheResultWithThePattern(){
         for (String guest : result) {
-            System.out.println("+--------------------------------------+");
-            System.out.println(guest);
-            System.out.println("+--------------------------------------+");
+            String[] splittedData = guest.split("\n");
+            String [] pattern = drawPatternWithValues(splittedData);
+            for (String line:pattern)
+                System.out.println(line);
         }
     }
 
